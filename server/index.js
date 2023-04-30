7beb311eaf94c7a2777418ffada7f367ed723952
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-04-30 21:01:00 +0300

    added auth routes backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,35 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+import authRoute from './routes/auth.js'
 
 const app = express()
+dotenv.config()
 
-app.listen(5001, () => {
-	console.log('Server started')
-})
+// Constants
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.use('/api/auth', authRoute)
+
+async function start() {
+	try	{
+		await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@blog.0dzvghy.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
+		app.listen(PORT, () => {
+			console.log(`Server started on port ${PORT}`)
+		})
+	} catch(error) {
+		console.log(error)
+	}
+}
+
+start()
