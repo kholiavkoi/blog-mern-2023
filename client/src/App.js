59a26d8c02fbd63471deb75ee698dbaa58d2cf78
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-04-30 23:20:39 +0300

    added store and register slice

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import MainPage from "./pages/MainPage";
+import PostsPage from "./pages/PostsPage";
+import PostPage from "./pages/PostPage";
+import AddPostPage from "./pages/AddPostPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import EditPostPage from "./pages/EditPostPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Layout>
+			<Routes>
+				<Route path='/' element={<MainPage />} />
+				<Route path='posts' element={<PostsPage />} />
+				<Route path=':id' element={<PostPage />} />
+				<Route path=':id/edit' element={<EditPostPage />} />
+				<Route path='new' element={<AddPostPage />} />
+				<Route path='register' element={<RegisterPage />} />
+				<Route path='login' element={<LoginPage />} />
+			</Routes>
+		</Layout>
+	)
 }
 
-export default App;
+export default App
